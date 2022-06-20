ed4eab5738ebec2184b7c85544ff66106c03ce0b
Matheus Girardi <girardi.matheus@hotmail.com>
2022-06-20 20:27:36 -0300

    fonts, icons, widget

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,9 +1,11 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/app/modules/splash/splash_page.dart';
 
 class AppModule extends Module {
-@override
-  List<Bind> binds = [];
+  @override
+  List<Bind> get binds => [];
 
   @override
-  List<ModularRoute> get routes => [];
+  List<ModularRoute> get routes =>
+      [ChildRoute('/', child: (context, args) => const SplashPage())];
 }
